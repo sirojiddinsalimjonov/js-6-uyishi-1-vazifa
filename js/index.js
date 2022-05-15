be56f6ba69dadea1184cededcaf3c181042af38a
sirojiddinsalimjonov <sirojiddinsalimjonovssz@gmail.com>
2022-05-15 11:26:19 +0500

    js armiya

---
+++ js/index.js
@@ -0,0 +1,15 @@
+alert ('Ro\'yxattan o\'ting')
+let firsName = prompt ('Iltimos ismingizni kiriting')
+let lastName = prompt ("Iltimos familyangizni kiriting")
+let year = prompt ("Tug\'ilgan yilingizni kiriting", '2022')
+let years = 2022 
+let userYear = years - year
+alert ('firstName' + '\n' + 'lastName' + '\n' + userYear)
+let teng = 18
+if (userYear > teng)
+{
+    alert('borasan chochima');
+}else if(userYear < teng) 
+{
+    alert('boromisan hapa boma');
+}
